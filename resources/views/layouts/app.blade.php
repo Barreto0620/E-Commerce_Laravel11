9e956433b690a72ad2b713798889f37865f9e233
lusxka <dev.lusxka@gmail.com>
2024-11-19 02:49:14 -0300

    cart

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -494,7 +494,7 @@
               </svg>
             </a>
 
-            <a href="cart.html" class="header-tools__item header-tools__cart">
+            <a href="cart" class="header-tools__item header-tools__cart">
               <svg class="d-block" width="20" height="20" viewBox="0 0 20 20" fill="none"
                 xmlns="http://www.w3.org/2000/svg">
                 <use href="#icon_cart" />
